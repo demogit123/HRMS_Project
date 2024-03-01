398ed1f3230dfdea886548d6bcd2026bae1bf697
Anil <Anil@DESKTOP-RT8SD5D>
2024-03-01 10:52:01 +0530

    Test case2 created

---
+++ GitProject/src/testcases/Testcase2.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class Testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test case 2");
+
+	}
+
+}
