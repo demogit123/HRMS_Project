f59e2422ceba6dc45f38b9f5dc947cb15fd0720d
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2024-03-01 10:55:40 +0530

    testCase2 modified

--- GitProject/src/testcases/Testcase2.java
+++ GitProject/src/testcases/Testcase2.java
@@ -5,6 +5,7 @@ public class Testcase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is test case 2");
+		System.out.println("TestCase 2 updated...");
 
 	}
 
